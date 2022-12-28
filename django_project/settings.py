df594bd211e49e4dbab22624e07559ed69e5e4b4
Armin Isakovic <59747818+Armin0001@users.noreply.github.com>
2022-12-28 12:08:57 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-website.herokuapp.com']
+ALLOWED_HOSTS = ['https://web-production-7f57.up.railway.app/']
 
 
 # Application definition
